345c6e1e4932ebe26bd7c06a71b9f4b216ec097d
AluneG <alune.greeve@hotmail.com>
2023-03-23 14:32:52 +0100

    A-G for decision variables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+from gurobipy import *
 
 with open('G18/G3/B.pickle', 'rb') as handle:
     B = pickle.load(handle)
@@ -7,12 +8,14 @@ with open('G18/G3/B.pickle', 'rb') as handle:
 with open('G18/G3/R.pickle', 'rb') as handle:
     R = pickle.load(handle)
 
+#Vertices
+V = [1,2]
 print(B)
 print(R)
 
 n = len(R)
 m = len(B)
-print(n)
+
 
 
 """ Parameters
@@ -21,6 +24,21 @@ R[i][i] #hi
 B[j][1][0] #Lj
 B[j][1][1] #Hj
 """
+
+#Create model
+
+m = Model('Cargo')
+
+#Decision variables
+
+
+A = [i for i in R]
+B = [k for k in R]
+C = [j for j in B]
+D = [(i,k) for i in R for k in R]
+E = [(i,j) for i in R for j in B]
+F = [(j,k) for j in B for k in R]
+G = [(i,k,l) for i in R for k in R for l in V]
 
 
 
