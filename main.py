f0a6dc2c96c6e35ad9806adb7c28451a4050d472
grjanssen <gijs.janssen09@gmail.com>
2023-03-23 10:23:58 +0100

    Data

---
+++ main.py
@@ -0,0 +1,11 @@
+import pickle
+
+
+with open('G18/G3/B.pickle', 'rb') as handle:
+    B = pickle.load(handle)
+
+with open('G18/G3/R.pickle', 'rb') as handle:
+    R = pickle.load(handle)
+
+print(B)
+print(R)
