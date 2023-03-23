dff0001aafc7b50cffdacebe6147e28ed7ad83e8
AluneG <alune.greeve@hotmail.com>
2023-03-23 14:12:55 +0100

    Parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pickle
-
+import numpy as np
 
 with open('G18/G3/B.pickle', 'rb') as handle:
     B = pickle.load(handle)
@@ -9,6 +9,19 @@ with open('G18/G3/R.pickle', 'rb') as handle:
 
 print(B)
 print(R)
-print(B[1])
+
+n = len(R)
+m = len(B)
+print(n)
+
+
+""" Parameters
+R[i][0] #li
+R[i][i] #hi
+B[j][1][0] #Lj
+B[j][1][1] #Hj
+"""
+
+
 
 
