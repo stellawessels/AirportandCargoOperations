1cf759e4db303409fa0ec81feb7c683c48451b60
WouterOffringa <wouter.offringa@outlook.com>
2023-03-23 14:42:14 +0100

    decision variables defined

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ with open('G18/G3/B.pickle', 'rb') as handle:
 with open('G18/G3/R.pickle', 'rb') as handle:
     R = pickle.load(handle)
 
-#Vertices
-V = [1,2]
+m = Model("Cargo")
+
 print(B)
 print(R)
 
@@ -18,19 +18,12 @@ m = len(B)
 
 
 
-""" Parameters
-R[i][0] #li
-R[i][i] #hi
-B[j][1][0] #Lj
-B[j][1][1] #Hj
-"""
-
-#Create model
-
-m = Model('Cargo')
-
-#Decision variables
-
+g = {}
+h = {}
+s = {}
+eta_1 = {}
+eta_3 = {}
+beta = {}
 
 A = [i for i in R]
 B = [k for k in R]
@@ -40,6 +33,19 @@ E = [(i,j) for i in R for j in B]
 F = [(j,k) for j in B for k in R]
 G = [(i,k,l) for i in R for k in R for l in V]
 
+#Create model
+
+m = Model('Cargo')
+
+#Decision variables
+
+
+g = m.addVars(A,vtype=GRB.BINARY, lb=0, name="g")
+h = m.addVars(D,vtype=GRB.BINARY, lb=0, name="h")
+s = m.addVars(D,vtype=GRB.BINARY, lb=0, name="s")
+eta_1 = m.addVars(D,vtype=GRB.BINARY, lb=0, name="eta_1")
+eta_2 = m.addVars(D,vtype=GRB.BINARY, lb=0, name="eta_2")
+beta = m.addVars(G,vtype=GRB.BINARY, lb=0, name="beta")
 
 
 
