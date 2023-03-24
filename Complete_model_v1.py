12786266036fcaed8282ae7e6a8ccecec5f4010b
AluneG <alune.greeve@hotmail.com>
2023-03-24 13:34:59 +0100

    U verplaatst

--- Complete_model_v1.py
+++ Complete_model_v1.py
@@ -86,6 +86,9 @@ for i in range(n):
         xp[i,k] = MILP.addVar(vtype=GRB.BINARY, name="xp_(%d,%d)"%(i,k))
         zp[i,k] = MILP.addVar(vtype=GRB.BINARY, name="zp_(%d,%d)"%(i,k))
 
+for j in range(m):
+    u[j] = MILP.addVar(vtype=GRB.BINARY, name="u_%d"%j)
+
 #TRYING: adding if-statement constraints for the conditional variables
 
 for i in range(n):
@@ -181,8 +184,7 @@ for i in range(n):
 
     MILP.addConstr(r[i, 3, 3], GRB.LESS_EQUAL, 1, name='Constraint 21')
 
-for j in range(m):
-    u[j] = MILP.addVar(vtype=GRB.BINARY, name="u_%d"%j)
+
 
 ##Vertical constraints
 
