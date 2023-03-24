429704e8754da65b5a03a6ccdbabce7dd14b72c2
AluneG <alune.greeve@hotmail.com>
2023-03-24 13:29:38 +0100

    Merged both models and everything done up to 13:30 24/3/2023

--- Complete_model_v1.py
+++ Complete_model_v1.py
@@ -2,44 +2,29 @@ import pickle
 import numpy as np
 from gurobipy import *
 
+###Get data from files
 with open('G18/G3/B.pickle', 'rb') as handle:
     B = pickle.load(handle)
 
 with open('G18/G3/R.pickle', 'rb') as handle:
     R = pickle.load(handle)
 
-#create model
+###Create model
 MILP = Model('Mixed Integer Linear Problem')
 
-
-
-#Vertices and other parameters
-V = [1,2]
-
-""" Parameters
+###Create parameters
+""" Parameters that should be called, not in a list
 R[i][0] #li
 R[i][i] #hi
+R[i][2] #l+
 B[j][1][0] #Lj
 B[j][1][1] #Hj
 """
 
-n = len(R)
-m = len(B)
+n = len(R) #number of boxes (25)
+m = len(B) #number of containers (4)
 
-per = np.zeros(len(R))
-ra = np.zeros(len(R))
-f = np.zeros(len(R))
-
-
-for item in R:
-    if R[item][4] == 1:
-        per[item] = 1
-    if R[item][5] == 1:
-        ra[item] = 1
-    if R[item][3] == 1:
-        f[item] = 1
-
-#L and H
+#Maximum height and length containers
 container_heights = []
 container_lengths = []
 for container in B:
@@ -49,7 +34,31 @@ for container in B:
 H = max(container_heights)
 L = max(container_lengths)
 
-#decision variables geometric
+#Create lists with 0 or 1 for perishable, radioactive and fragile
+per = np.zeros(len(R))
+ra = np.zeros(len(R))
+f = np.zeros(len(R))
+
+for item in R:
+    if R[item][4] == 1:
+        per[item] = 1
+    if R[item][5] == 1:
+        ra[item] = 1
+    if R[item][3] == 1:
+        f[item] = 1
+
+#Create lists of sides and axes
+a = [1,3]
+b = [1,3]
+#Create list of all vertices
+V = [1,2]
+
+###Creating decision variables
+
+##Decision variables geometric contstraints
+
+#Empty decision variables
+
 p = {}
 u = {}
 x = {}
@@ -60,33 +69,52 @@ r = {}
 xp = {}
 zp = {}
 
-a = [1,3]
-b = [1,3]
+#Create decision variables
 
 for i in range(n):
-    x[i] = MILP.addVar(vtype=GRB.CONTINUOUS)
-    z[i] = MILP.addVar(vtype=GRB.CONTINUOUS)
-    x_prime[i] = MILP.addVar(vtype=GRB.CONTINUOUS)
-    z_prime[i] = MILP.addVar(vtype=GRB.CONTINUOUS)
+    x[i] = MILP.addVar(vtype=GRB.CONTINUOUS, name="x_%d"%i)
+    z[i] = MILP.addVar(vtype=GRB.CONTINUOUS, name="z_%d"%i)
+    x_prime[i] = MILP.addVar(vtype=GRB.CONTINUOUS, name="x_prime_%d"%i)
+    z_prime[i] = MILP.addVar(vtype=GRB.CONTINUOUS, name="z_prime_%d"%i)
     for j in range(m):
-        p[i, j] = MILP.addVar(vtype=GRB.BINARY)
-        u[j] = MILP.addVar(vtype=GRB.BINARY)
+        p[i,j] = MILP.addVar(vtype=GRB.BINARY, name="p_(%d,%d)"%(i,j))
     for aa, value_a in enumerate(a):
         for bb, value_b in enumerate(b):
-            r[i, value_a, value_b] = MILP.addVar(vtype=GRB.BINARY)
+            r[i, value_a, value_b] = MILP.addVar(vtype=GRB.BINARY, name="r_(%d,%d,%d)"%(i, value_a, value_b))
 
     for k in range(n):
-        xp[i, k] = MILP.addVar(vtype=GRB.BINARY)
-        zp[i, k] = MILP.addVar(vtype=GRB.BINARY)
+        xp[i,k] = MILP.addVar(vtype=GRB.BINARY, name="xp_(%d,%d)"%(i,k))
+        zp[i,k] = MILP.addVar(vtype=GRB.BINARY, name="zp_(%d,%d)"%(i,k))
 
-##Volgens mij zijn deze niet nodig
-# g = {}
-# h = {}
-# s = {}
-# eta_1 = {}
-# eta_3 = {}
-# beta = {}
+#TRYING: adding if-statement constraints for the conditional variables
 
+for i in range(n):
+    for k in range(n):
+        if MILP.addConstr(x_prime[k] <= x[i], name="Trial constraint x"):
+            xp[i,k] = 1
+        else:
+            xp[i, k] = 0
+
+for i in range(n):
+    for k in range(n):
+        if MILP.addConstr(z_prime[k] <= z[i], name="Trial constraint z"):
+            zp[i,k] = 1
+        else:
+            zp[i, k] = 0
+
+##Decision variables verticle constraints
+
+#Create empty lists for decision variables
+g = {}
+h = {}
+s = {}
+eta_1 = {}
+eta_3 = {}
+beta = {}
+v = {}
+m_ik = {}
+
+#Define the for-loops for which the decision variables will be used
 A = [i for i in R]
 B = [k for k in R]
 C = [j for j in B]
@@ -96,8 +124,7 @@ F = [(j, k) for j in B for k in R]
 G = [(i, k, ver) for i in R for k in R for ver in V]
 
 
-#Decision variables
-
+#Create decision variables
 
 g = MILP.addVars(A,vtype=GRB.BINARY, lb=0, name="g")
 h = MILP.addVars(D,vtype=GRB.BINARY, lb=0, name="h")
@@ -108,8 +135,56 @@ beta = MILP.addVars(G,vtype=GRB.BINARY, lb=0, name="beta")
 v = MILP.addVars(D, vtype=GRB.CONTINUOUS, lb =0, name="v")
 m_ik = MILP.addVars(D, vtype=GRB.BINARY, lb=0, name='m_ik')
 
+###Create objective function
 
-#Constraints
+obj = quicksum(u[j] * B[j][1][3] for j in range(m))
+
+MILP.setObjective(obj, GRB.MINIMIZE)
+MILP.update()
+
+###Create constraints
+
+##Geometric constraints
+
+
+for j in range(m):
+    MILP.addConstr(quicksum(p[i,j] for i in range(n)), GRB.LESS_EQUAL, n * u[j])
+
+for i in range(n):
+    MILP.addConstr(quicksum(p[i,j] for j in range(m)), GRB.EQUAL, 1, name='Constraint 4')
+
+    MILP.addConstr(x_prime[i], GRB.LESS_EQUAL, quicksum(B[j][1][0] * p[i,j] for j in range(m)), name='Constraint 5')
+
+    MILP.addConstr(z_prime[i], GRB.LESS_EQUAL, quicksum(B[j][1][1] * p[i, j] for j in range(m)), name='Constraint 7')
+
+    MILP.addConstr(x_prime[i] - x[i], GRB.EQUAL, r[i,1,1] * R[i][0] + r[i,1,3] * R[i][1], name='Constraint 8')
+    MILP.addConstr(z_prime[i] - z[i], GRB.EQUAL, r[i,3,1] * R[i][0] + r[i,3,3] * R[i][1], name='Constraint 10')
+
+    for bb, value_b in enumerate(b):
+        MILP.addConstr(quicksum(r[i,value_a,value_b] for aa, value_a in enumerate(a)), GRB.EQUAL, 1, name='Constraint 11')
+
+    for aa, value_a in enumerate(a):
+        MILP.addConstr(quicksum(r[i,value_a,value_b] for bb, value_b in enumerate(b)), GRB.EQUAL, 1, name='Constraint 12')
+
+    for j in range(m):
+        for k in range(n):
+            MILP.addConstr(xp[i,k] + xp[k,i] + zp[i,k] + zp[k,i], GRB.GREATER_EQUAL, (p[i,j] + p[k,j]) - 1, name='Constraint 13')
+
+            MILP.addConstr(x_prime[k], GRB.LESS_EQUAL, x[i] + (1-xp[i,k]) * L, name='Constraint 14')
+
+            MILP.addConstr(x[i] + 1, GRB.LESS_EQUAL, x_prime[k] + xp[i,k] * L, name='Constraint 15')
+
+            MILP.addConstr(z_prime[k], GRB.LESS_EQUAL, z[i] + (1 - zp[i, k]) * H, name='Constraint 18')
+
+
+    MILP.addConstr(r[i,3,1], GRB.LESS_EQUAL, R[i][2], name='Constraint 19')
+
+    MILP.addConstr(r[i, 3, 3], GRB.LESS_EQUAL, 1, name='Constraint 21')
+
+for j in range(m):
+    u[j] = MILP.addVar(vtype=GRB.BINARY, name="u_%d"%j)
+
+##Vertical constraints
 
 #Constraint 26
 
@@ -201,3 +276,14 @@ for i in R:
         for k in R:
             for j in B:
                 MILP.addConstr(ra[i]*per[k], GRB.LESS_EQUAL, 2 - p[i][j] - p[k][j], name='CPR')
+
+###Solve the MILP
+
+MILP.update()
+MILP.write('P4RMP_LP.lp')
+
+MILP.optimize()
+
+#Write solution file
+
+MILP.write('P4RMPSolution.sol')
