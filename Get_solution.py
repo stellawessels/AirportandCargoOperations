b9ba721d77c81bc3d1afcf8655c15356a13db835
AluneG <alune.greeve@hotmail.com>
2023-03-31 10:18:07 +0200

    Get output from programme

--- Get_solution.py
+++ Get_solution.py
@@ -1,26 +1,42 @@
 import pandas as pd
 import numpy as np
+import pickle
+
+###Get data from files
+with open('G18/G3/B.pickle', 'rb') as handle:
+    B = pickle.load(handle)
+
+with open('G18/G3/R.pickle', 'rb') as handle:
+    R = pickle.load(handle)
+
 with open("P4RMPSolution.sol") as f:
     content = f.read()
 # with open('G18/G3/R.pickle', 'rb') as handle:
 #     R = pickle.load(handle)
 
-R = 24
 lines = content.split('\n')
 
-
-keylist = np.arange(0,R+1,1)
+length_R = 24
+keylist = np.arange(0,length_R +1,1)
 answers = {}
 for i in keylist:
-    answers[i] = None
+    answers.setdefault(i, [])
 for line, value in enumerate(lines):
-    for i in range(R+1):
+    for i in range(length_R+1):
         newvalue = value.split()
         if len(newvalue) != 0:
             if newvalue[0] == 'x_'+ str(i):
-                answers[i].append(newvalue[1])
+                answers[i].append(float(newvalue[1]))
             if newvalue[0] == 'z_'+ str(i):
-                answers[i].append(newvalue[1])
-print(answers)
+                answers[i].append(float(newvalue[1]))
+            if newvalue[0] == 'x_prime_' + str(i):
+                answers[i].append(float(newvalue[1]) - float(answers[i][0]))
+            if newvalue[0] == 'z_prime_' + str(i):
+                answers[i].append(float(newvalue[1]) - float(answers[i][1]))
 
 
+
+for i in range(length_R+1):
+    answers[i].append(R[i][3])
+    answers[i].append(R[i][4])
+    answers[i].append(R[i][5])
